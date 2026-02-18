1e2bdb6fb6743b393c1ca9f9125b8be3714b0c83
dsathyajith <meeriyagallad@gmail.com>
2026-02-18 23:34:43 +0530

    .

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
+
+  base: command === "serve" ? "/" : "/Oshadhi-Vanodhya-Portfolio/",
+
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
     alias: {
